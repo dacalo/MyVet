f9ec2733ba874a5b260a549aaa83b47149015e47
David Chávez López <dacalo.soporte@gmail.com>
2021-10-11 18:56:38 -0500

    Delete comments

--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -26,7 +26,6 @@ namespace MyVet.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureApplicationCookie(options =>
@@ -37,7 +36,6 @@ namespace MyVet.Web
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
